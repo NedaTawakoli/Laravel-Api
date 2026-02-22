feda33da50ef00f31c713a3709aeed1e9e037656
NedaTawakoli <tawakolineda@gmail.com>
2026-02-22 23:04:59 +0430

    factory book added

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Author;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,6 +19,18 @@ class BookFactory extends Factory
     {
         return [
             //
+            // Author::inRandomOrder()->first()?->id() ?? Author::factory()
+            "title"=>$this->faker->sentence(3),
+            "isbn"=>$this->faker->unique()->isbn13(),
+            "description"=>$this->faker->paragraph(),
+            "author_id"=>$this->faker->randomElement([1,3]),
+            "genre"=>$this->faker->randomElement(['fiction','motivational','non-fiction','Fantasy','Novel']),
+            "published_at"=>$this->faker->date(),
+            "total_copies"=>$this->faker->numberBetween(1,30),
+            "available_copies"=>$this->faker->numberBetween(1,30),
+            "price"=>$this->faker->randomFloat(2,50,90),
+            "cover_image"=>$this->faker->imageUrl('200','300','books'.true),
+            "status"=>$this->faker->randomElement(['available','unavailable'])
         ];
     }
 }
