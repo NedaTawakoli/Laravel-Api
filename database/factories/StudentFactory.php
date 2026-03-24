3951cac95187e271d069f97bb20812a1a47bca9d
NedaTawakoli <tawakolineda@gmail.com>
2026-03-24 23:24:13 +0430

    students factory added

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            "name"=>$this->faker->name(),
+            "lastName"=>$this->faker->lastName(),
+            "Date-of-birth"=>$this->faker->date(),
+            "gender"=>$this->faker->randomElement(["f","m"]),
+             "Age"=>$this->faker->numberBetween(16,50),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Book;
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
          Book::factory(40)->create();
+         Student::factory(20)->create();
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
